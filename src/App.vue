6c4057d8d698509330158fa73fa6c149b9cd6413
iTzMvz <matheus.vidal.zenere@gmail.com>
2023-12-12 11:30:20 -0300

    dasd

--- src/App.vue
+++ src/App.vue
@@ -6,163 +6,183 @@ import { ref } from 'vue'
 const menuToggle = ref(false)
 
 function toggleMenu() {
-  menuToggle.value == true ? (menuToggle.value = false) : (menuToggle.value = true)
+ menuToggle.value == true ? (menuToggle.value = false) : (menuToggle.value = true)
 }
 </script>
 <template>
-  <header>
-    <div :class="menuToggle == true ? 'menuOn active' : 'menuOn'">
-      <router-link to="/tabela">
-<img src="https://i.ibb.co/M21tp39/COPAO.png" alt="" class="logoMenu"  />      </router-link>
+ <header>
+ <div :class="menuToggle == true ? 'menuOn active' : 'menuOn'">
+ <router-link to="/tabela">
+<img src="https://i.ibb.co/M21tp39/COPAO.png" alt="" class="logoMenu" /> </router-link>
 
-      <ul>
-        <li>
-          <span class="menuElement" @click="toggleMenu()"><router-link to="/jogadores">jogadores</router-link></span>
-        </li>
-        <li>
-          <span class="menuElement" @click="toggleMenu()"><router-link to="/tabela">times</router-link></span>
-        </li>
-        <li>
-          <span class="menuElement" @click="toggleMenu()"><router-link to="/artilharia">artilharia</router-link></span>
-        </li>
-        <li>
-          <span class="menuElement" @click="toggleMenu()"><router-link to="/chaveamento">chaveamento</router-link></span>
-        </li>
-        <li>
-          <span class="menuElement" @click="toggleMenu()"><router-link to="/jogos">jogos</router-link></span>
-        </li>
-      </ul>
-    </div>
-    <div @click="menuToggle = false" :class="menuToggle == true ? 'teste1' : 'teste2'"></div>
-    <div class="container">
-      <Menu @click="toggleMenu()" class="icons" size="30" />
-      <router-link to="/tabela">
-        <img class="logo" src="https://i.ibb.co/M21tp39/COPAO.png" alt="" />
-      </router-link>
-      <div></div>
-    </div>
-    <div class="sub-header">
-      <div class="sub-container">
-        <router-link to="/tabela">Classificação</router-link>
-        <router-link to="/artilharia">Artilharia</router-link>
-        <router-link to="/chaveamento">Chaveamento</router-link>
-        <router-link to="/jogos">Jogos</router-link>
-      </div>
-    </div>
-  </header>
-  <RouterView />
+ <ul>
+ <li>
+ <span class="menuElement" @click="toggleMenu()"><router-link to="/jogadores">jogadores</router-link></span>
+ </li>
+ <li>
+ <span class="menuElement" @click="toggleMenu()"><router-link to="/tabela">times</router-link></span>
+ </li>
+ <li>
+ <span class="menuElement" @click="toggleMenu()"><router-link to="/artilharia">artilharia</router-link></span>
+ </li>
+ <li>
+ <span class="menuElement" @click="toggleMenu()"><router-link to="/chaveamento">chaveamento</router-link></span>
+ </li>
+ <li>
+ <span class="menuElement" @click="toggleMenu()"><router-link to="/jogos">jogos</router-link></span>
+ </li>
+ </ul>
+ </div>
+ <div @click="menuToggle = false" :class="menuToggle == true ? 'teste1' : 'teste2'"></div>
+ <div class="container">
+ <Menu @click="toggleMenu()" class="icons" size="30" />
+ <router-link to="/tabela">
+ <img class="logo" src="https://i.ibb.co/M21tp39/COPAO.png" alt="" />
+ </router-link>
+ <div></div>
+ </div>
+ <div class="sub-header">
+ <div class="sub-container">
+ <router-link to="/tabela">Classificação</router-link>
+ <router-link to="/artilharia">Artilharia</router-link>
+ <router-link to="/chaveamento">Chaveamento</router-link>
+ <router-link to="/jogos">Jogos</router-link>
+ </div>
+ </div>
+ </header>
+ <main>
+ <RouterView />
+ </main>
+ 
 </template>
 <style scoped>
 .teste2 {
-  visibility: hidden;
-  z-index: 1;
-  top: 0;
-  left: 0;
-  position: absolute;
-  height: 100vh;
-  width: 100vw;
-  transition: 0.4s ease-in-out;
+ visibility: hidden;
+ z-index: 1;
+ top: 0;
+ left: 0;
+ position: absolute;
+ height: 100vh;
+ width: 100vw;
+ transition: 0.4s ease-in-out;
 }
 .logoMenu{
-  width: 18vw;
+ width: 18vw;
 }
 .teste1 {
-  z-index: 1;
-  top: 0;
-  left: 0;
-  position: absolute;
-  height: 100vh;
-  width: 100vw;
-  background-color: rgba(0, 0, 0, 0.596);
-  transition: 0.4s ease-in-out;
+ z-index: 1;
+ top: 0;
+ left: 0;
+ position: absolute;
+ height: 100vh;
+ width: 100vw;
+ background-color: rgba(0, 0, 0, 0.596);
+ transition: 0.4s ease-in-out;
 }
 .icons {
-  cursor: pointer;
+ cursor: pointer;
 }
 .menuOn {
-  z-index: 2;
-  left: -500px;
-  position: absolute;
-  transition: 0.2s ease-in-out;
-  height: 100vh;
-  width: 18vw;
+ z-index: 2;
+ left: -500px;
+ position: absolute;
+ transition: 0.2s ease-in-out;
+ height: 100vh;
+ width: 18vw;
 }
 .menuOn.active {
-  z-index: 2;
-  position: absolute;
-  height: 100vh;
-  width: 18vw;
-  background-color: rgb(255, 255, 255);
-  transition: 0.4s ease-in-out;
-  left: 0%;
-  box-shadow: 6px 0px 10px rgba(0, 0, 0, 0.384);
+ z-index: 2;
+ position: absolute;
+ height: 100vh;
+ width: 18vw;
+ background-color: rgb(255, 255, 255);
+ transition: 0.4s ease-in-out;
+ left: 0%;
+ box-shadow: 6px 0px 10px rgba(0, 0, 0, 0.384);
 }
 .menuOn li {
-  text-decoration: none;
-  font-size: 1.3em;
+ text-decoration: none;
+ font-size: 1.3em;
  
 }
 a{
-  color: rgb(39, 39, 39);
+ color: rgb(39, 39, 39);
 }
 .menuElement {
-  padding-left: 1vw;
+ padding-left: 1vw;
 }
 li :hover {
-  cursor: pointer;
-  color: rgb(0, 0, 0);
-  transition: .2s;
+ cursor: pointer;
+ color: rgb(0, 0, 0);
+ transition: .2s;
 
 }
 li :active {
-  color: green;
+ color: green;
 
 }
 .menuOn li:first-child {
-  padding-top: 5vh;
+ padding-top: 5vh;
 }
 
 ul {
-  list-style-type: none;
-  margin: 0;
-  padding: 0;
-  gap: 2vw;
-  display: flex;
-  flex-direction: column;
+ list-style-type: none;
+ margin: 0;
+ padding: 0;
+ gap: 2vw;
+ display: flex;
+ flex-direction: column;
 }
 header {
-  background-image: radial-gradient(#17842a 20%, #096318 80%);
-  display: flex;
-  flex-direction: column;
-  width: 100vw;
+ background-image: radial-gradient(#17842a 20%, #096318 80%);
+ display: flex;
+ flex-direction: column;
+ width: 100vw;
 }
 .container {
-  width: 60.94vw;
-  height: 13.89vh;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-right: auto;
-  margin-left: auto;
+ width: 60.94vw;
+ height: 13.89vh;
+ display: flex;
+ justify-content: space-between;
+ align-items: center;
+ margin-right: auto;
+ margin-left: auto;
 }
 .sub-header {
-  display: flex;
-  height: 3.7vh;
-  background: #00420c;
-  width: 100vw;
+ display: flex;
+ height: 3.7vh;
+ background: #00420c;
+ width: 100vw;
 }
 .logo {
-  width: 25vw;
+ width: 25vw;
 }
 .sub-container {
-  width: 60.94vw;
-  display: flex;
-  align-items: center;
-  gap: 2vw;
-  margin-right: auto;
-  margin-left: auto;
-}
+ width: 60.94vw;
+ display: flex;
+ align-items: center;
+ gap: 2vw;
+ margin-right: auto;
+ margin-left: auto;
+ justify-content: center;
+} 
 .sub-container a {
-  color: white;
+ color: white;
+}
+
+main {
+ width: 100vw;
+}
+@media (max-width: 600px){
+ .sub-container{
+ display: none;
+ }
+ .menuOn.active{
+ width: 80%;
+ }
+ ul{
+ justify-content: center;
+ margin-left: 10%;
+ }
 }
 </style>
--- src/views/TabelaView.vue
+++ src/views/TabelaView.vue
@@ -4,219 +4,246 @@ import { db } from '@/firebase'
 import { collection, getDocs } from 'firebase/firestore'
 
 const tabela = ref([])
+const scroll = ref(false)
+
+function handleScroll(e) {
+ if (e.target.scrollLeft > 0) {
+ scroll.value = true
+ } else {
+ scroll.value = false
+ }
+}
 
 onMounted(async () => {
-  const querySnapshot = await getDocs(collection(db, 'times'))
-  let test = []
-  querySnapshot.forEach((doc) => {
-    const time = {
-      id: doc.id,
-      nomeTime: doc.data().nomeTime.toUpperCase(),
-      vitorias: doc.data().vitorias,
-      derrotas: doc.data().derrotas,
-      empates: doc.data().empates,
-      jogos: doc.data().jogos,
-      gols_pro: doc.data().golsPro,
-      gols_contra: doc.data().golsContra,
-      aproveitamento: doc.data().porcentagem,
-      escudo: doc.data().escudo,
-      pontos: doc.data().vitorias * 3 + doc.data().empates,
-      ultJogos: doc.data().ultJogos
-      
-    }
-    test.push(time)
-  })
-  tabela.value = test
-  console.log(tabela.value)
+ const querySnapshot = await getDocs(collection(db, 'times'))
+ let test = []
+ querySnapshot.forEach((doc) => {
+ const time = {
+ id: doc.id,
+ nomeTime: doc.data().nomeTime.toUpperCase(),
+ vitorias: doc.data().vitorias,
+ derrotas: doc.data().derrotas,
+ empates: doc.data().empates,
+ jogos: doc.data().jogos,
+ gols_pro: doc.data().golsPro,
+ gols_contra: doc.data().golsContra,
+ aproveitamento: doc.data().porcentagem,
+ escudo: doc.data().escudo,
+ pontos: doc.data().vitorias * 3 + doc.data().empates,
+ ultJogos: doc.data().ultJogos
+ 
+ }
+ test.push(time)
+ })
+ tabela.value = test
+ console.log(tabela.value)
 })
 </script>
 <template>
-  <div class="table-page">
-  
-    <h1>TABELA</h1>
-    <table>
-      <thead>
-        <tr>
-          <th>Classificação</th>
-          <th>P</th>
-          <th>J</th>
-          <th>V</th>
-          <th>E</th>
-          <th>D</th>
-          <th>GP</th>
-          <th>GC</th>
-          <th>SG</th>
-          <th>%</th>
-          <th>ult. jogos</th>
-        </tr>
-      </thead>
-      <tbody>
-        <!-- <tr v-for="time in tabela.sort((a, b) => a.pontos == b.pontos ? (b.vitorias * 3 + b.empates + (b.gols_pro - b.gols_contra)) - (a.vitorias * 3 + a.empates + a.gols_pro - a.gols_contra) : b.pontos - a.pontos).slice(0, 8)" :key="time.id">
-          <td style="font-weight: 900">
-            <div class="nomeTime-box">
-              <span>{{ tabela.indexOf(time) + 1 }}º </span>
-              <span
-                :class="
-                  (tabela.indexOf(time) + 1) <= 8 && (tabela.indexOf(time)) + 1 >= 6
-                    ? 'zona-r'
-                    : (tabela.indexOf(time) + 1) <= 5 && (tabela.indexOf(time) + 1) >= 4
-                    ? 'zona-n'
-                    : 'zona-c'
-                "
-              >
-              </span>
+ 
+ <div class="classificacao">
+ <h1>TABELA</h1>
+ <div class="table-page" @scroll.passive="handleScroll" >
+ <table>
+ <thead>
+ <tr>
+ <th>{{ scroll ? 'POS' : 'Posição' }}</th>
+ <th>P</th>
+ <th>J</th>
+ <th>V</th>
+ <th>E</th>
+ <th>D</th>
+ <th>GP</th>
+ <th>GC</th>
+ <th>SG</th>
+ <th>%</th>
+ <th>ult. jogos</th>
+ </tr>
+ </thead>
+ <tbody>
+ <!-- <tr v-for="time in tabela.sort((a, b) => a.pontos == b.pontos ? (b.vitorias * 3 + b.empates + (b.gols_pro - b.gols_contra)) - (a.vitorias * 3 + a.empates + a.gols_pro - a.gols_contra) : b.pontos - a.pontos).slice(0, 8)" :key="time.id">
+ <td style="font-weight: 900">
+ <div class="nomeTime-box">
+ <span>{{ tabela.indexOf(time) + 1 }}º </span>
+ <span
+ :class="
+ (tabela.indexOf(time) + 1) <= 8 && (tabela.indexOf(time)) + 1 >= 6
+ ? 'zona-r'
+ : (tabela.indexOf(time) + 1) <= 5 && (tabela.indexOf(time) + 1) >= 4
+ ? 'zona-n'
+ : 'zona-c'
+ "
+ >
+ </span>
 
-              <span style="display: flex;"><img style="width: 4vh" :src="time.escudo" alt="" /></span>
+ <span style="display: flex;"><img style="width: 4vh" :src="time.escudo" alt="" /></span>
 
-              <span>{{ time.nomeTime }}</span>
-            </div>
-          </td>
-          <td class="marks">{{ (time.vitorias * 3) + (time.empates) }}</td>
-          <td>{{ time.jogos }}</td>
-          <td class="marks">{{ time.vitorias }}</td>
-          <td>{{ time.empates }}</td>
-          <td class="marks">{{ time.derrotas }}</td>
-          <td>{{ time.gols_pro }}</td>
-          <td class="marks">{{ time.gols_contra }}</td>
-          <td>{{ time.gols_pro - time.gols_contra }}</td>
-          <td class="marks">{{ Number(time.vitorias) / Number(time.jogos) * 100 }}</td>
-          <td class="ult_jogos">
-            <span
-              v-for="jogo in time.ultimos_jogos"
-              :key="jogo.legth"
-              :class="jogo == 'v' ? 'v' : jogo == 'd' ? 'd' : 'e'"
-            >
-            </span>
-          </td>
-        </tr>
-      </tbody> -->
-     <tr v-for="time in tabela.sort((a,b) => a.pontos == b.pontos ? (b.pontos + b.gols_pro - b.gols_contra) - (a.pontos + a.gols_pro - a.gols_contra): b.pontos - a.pontos).slice(0, 8)" :key="time.id">
-          <td style="font-weight: 900">
-            <div class="nomeTime-box">
-              <span>{{ tabela.indexOf(time) + 1 }}º </span>
-              <span
-                :class="
-                  (tabela.indexOf(time) + 1) <= 8 && (tabela.indexOf(time)) + 1 >= 6
-                    ? 'zona-r'
-                    : (tabela.indexOf(time) + 1) <= 5 && (tabela.indexOf(time) + 1) >= 4
-                    ? 'zona-n'
-                    : 'zona-c'
-                "
-              >
-              </span>
+ <span>{{ time.nomeTime }}</span>
+ </div>
+ </td>
+ <td class="marks">{{ (time.vitorias * 3) + (time.empates) }}</td>
+ <td>{{ time.jogos }}</td>
+ <td class="marks">{{ time.vitorias }}</td>
+ <td>{{ time.empates }}</td>
+ <td class="marks">{{ time.derrotas }}</td>
+ <td>{{ time.gols_pro }}</td>
+ <td class="marks">{{ time.gols_contra }}</td>
+ <td>{{ time.gols_pro - time.gols_contra }}</td>
+ <td class="marks">{{ Number(time.vitorias) / Number(time.jogos) * 100 }}</td>
+ <td class="ult_jogos">
+ <span
+ v-for="jogo in time.ultimos_jogos"
+ :key="jogo.legth"
+ :class="jogo == 'v' ? 'v' : jogo == 'd' ? 'd' : 'e'"
+ >
+ </span>
+ </td>
+ </tr>
+ </tbody> -->
+ <tr v-for="time in tabela.sort((a,b) => a.pontos == b.pontos ? (b.pontos + b.gols_pro - b.gols_contra) - (a.pontos + a.gols_pro - a.gols_contra): b.pontos - a.pontos).slice(0, 8)" :key="time.id">
+ <td style="font-weight: 900">
+ <div class="nomeTime-box">
+ <span>{{ tabela.indexOf(time) + 1 }}º </span>
+ <span
+ :class="
+ (tabela.indexOf(time) + 1) <= 8 && (tabela.indexOf(time)) + 1 >= 6
+ ? 'zona-r'
+ : (tabela.indexOf(time) + 1) <= 5 && (tabela.indexOf(time) + 1) >= 4
+ ? 'zona-n'
+ : 'zona-c'
+ "
+ >
+ </span>
 
-              <span style="display: flex;"><img style="width: 4vh" :src="time.escudo" alt="" /></span>
+ <span style="display: flex;"><img style="width: 4vh" :src="time.escudo" alt="" /></span>
 
-              <span>{{ time.nomeTime }}</span>
-            </div>
-          </td>
-          <td class="marks">{{ (time.pontos) }}</td>
-          <td>{{ time.jogos }}</td>
-          <td class="marks">{{ time.vitorias }}</td>
-          <td>{{ time.empates }}</td>
-          <td class="marks">{{ time.derrotas }}</td>
-          <td>{{ time.gols_pro }}</td>
-          <td class="marks">{{ time.gols_contra }}</td>
-          <td>{{ time.gols_pro - time.gols_contra }}</td>
-          <td class="marks">{{ Number(time.vitorias) / Number(time.jogos) * 100 }}</td>
-          <td class="ult_jogos">
-            <span
-              v-for="jogo in time.ultJogos"
-              :key="jogo.legth"
-              :class="jogo == 'v' ? 'v' : jogo == 'd' ? 'd' : 'e'"
-            >
-            </span>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
+ <span>{{ scroll ? '' : time.nomeTime }}</span>
+ </div>
+ </td>
+ <td class="marks">{{ (time.pontos) }}</td>
+ <td>{{ time.jogos }}</td>
+ <td class="marks">{{ time.vitorias }}</td>
+ <td>{{ time.empates }}</td>
+ <td class="marks">{{ time.derrotas }}</td>
+ <td>{{ time.gols_pro }}</td>
+ <td class="marks">{{ time.gols_contra }}</td>
+ <td>{{ time.gols_pro - time.gols_contra }}</td>
+ <td class="marks">{{ Number(time.vitorias) / Number(time.jogos) * 100 }}</td>
+ <td class="ult_jogos">
+ <span
+ v-for="jogo in time.ultJogos"
+ :key="jogo.legth"
+ :class="jogo == 'v' ? 'v' : jogo == 'd' ? 'd' : 'e'"
+ >
+ </span>
+ </td>
+ </tr>
+ </tbody>
+ </table>
+ </div >
+</div>
 </template>
 <style scoped>
 h1 {
-  padding: 54px 0px 0px 0px;
-  text-align: start;
-  font-weight: 900;
+ padding: 54px 0px 0px 0px;
+ text-align: start;
+ font-weight: 900;
 }
 
 .nomeTime-box {
-  display: flex;
-  align-items: center;
-  gap: 1vw;
+ display: flex;
+ align-items: center;
+ gap: 1vw;
 }
 .zona-r {
-  display: block;
-  width: 2px;
-  height: 2vh;
-  background-color: red;
+ display: block;
+ width: 2px;
+ height: 2vh;
+ background-color: red;
 }
 .zona-n {
-  display: block;
-  width: 2px;
-  height: 2vh;
-  background-color: grey;
+ display: block;
+ width: 2px;
+ height: 2vh;
+ background-color: grey;
 }
 .zona-c {
-  display: block;
-  width: 2px;
-  height: 2vh;
-  background-color: green;
+ display: block;
+ width: 2px;
+ height: 2vh;
+ background-color: green;
 }
 .ult_jogos {
-  display: flex;
-  height: 6vh;
-  align-items: center;
-  justify-content: space-evenly;
+ display: flex;
+ height: 6vh;
+ align-items: center;
+ justify-content: space-evenly;
 }
 .v {
-  width: 1.1vh;
-  height: 1.1vh;
-  background-color: green;
-  border-radius: 50%;
-  content: '';
-  display: block;
+ width: 1.1vh;
+ height: 1.1vh;
+ background-color: green;
+ border-radius: 50%;
+ content: '';
+ display: block;
 }
 .e {
-  width: 1.1vh;
-  height: 1.1vh;
-  background-color: grey;
-  border-radius: 50%;
-  content: '';
-  display: block;
+ width: 1.1vh;
+ height: 1.1vh;
+ background-color: grey;
+ border-radius: 50%;
+ content: '';
+ display: block;
 }
 .d {
-  width: 1.1vh;
-  height: 1.1vh;
-  background-color: rgb(128, 0, 0);
-  border-radius: 50%;
-  content: '';
-  display: block;
+ width: 1.1vh;
+ height: 1.1vh;
+ background-color: rgb(128, 0, 0);
+ border-radius: 50%;
+ content: '';
+ display: block;
 }
 
 .table-page {
-  width: 60.94vw;
-  display: flex;
-  flex-direction: column;
-  margin-right: auto;
-  margin-left: auto;
+ overflow-x:auto;
 }
 tr {
-  text-align: center;
-  border-bottom: solid 1px #ddd;
-  height: 6vh;
+ text-align: center;
+ border-bottom: solid 1px #ddd;
+ height: 6vh;
 }
 
 table {
-  border-top: 1px solid #ddd;
-  border-collapse: collapse;
-}
+ border-top: 1px solid #ddd;
+ border-collapse: collapse;
+ overflow-x: scroll;
+ overflow-y: scroll;
+ }
 .marks {
-  background-color: #e9e9e9;
+ background-color: #e9e9e9;
+}
+
+table tr td:first-child {
+ position: sticky; 
+ background-color: white;
+ left: 0;
+}
+
+table thead tr th:first-child {
+ position: sticky; 
+ background-color: white;
+ left: 0;
 }
 
 th {
-  padding: 5px 10px;
+ padding: 5px 10px;
+}
+@media (min-width: 600px){
+ .classificacao{
+ display: flex;
+ justify-content: center;
+ flex-direction: column ;
+ align-items: center;
+ }
 }
 </style>
-
-
